33bfc56203d59cd3521c8464311e5cd945f9c260
leongaban <leongaban@gmail.com>
2023-09-21 20:51:52 -0500

    rebuilt with LeonTheme

---
+++ dist/lib/es6/components/LeonTheme/index.js
@@ -0,0 +1,2 @@
+export { default } from "./LeonTheme";
+//# sourceMappingURL=index.js.map
--- dist/lib/es6/components/index.js
+++ dist/lib/es6/components/index.js
@@ -1,2 +1,3 @@
 export { default as Button } from "./Button";
+export { default as LeonTheme } from "./LeonTheme";
 //# sourceMappingURL=index.js.map
