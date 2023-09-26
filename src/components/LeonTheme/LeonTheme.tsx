f3435a7945c8c3b65019877034870468ca84684d
leongaban <leongaban@gmail.com>
2023-09-26 12:53:43 -0500

    added moduleResolution node and removed console log

--- src/components/LeonTheme/LeonTheme.tsx
+++ src/components/LeonTheme/LeonTheme.tsx
@@ -1,30 +1,28 @@
-import React from "react";
-import { ThemeProvider } from "styled-components";
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
 
-import { myTheme } from "../../theme";
+import { myTheme } from '../../theme'
 
 export interface myThemeInterface {
-  borderRadius: string;
-  color: string;
-  backgroundColor: string;
+  borderRadius: string
+  color: string
+  backgroundColor: string
 }
 
 interface LeonThemeProps {
-  component?: any;
-  theme: myThemeInterface;
+  component?: any
+  theme: myThemeInterface
 }
 
 const LeonTheme = (props: LeonThemeProps) => {
-  const Component = props.component;
-  const themeStyle = props.theme ? props.theme : myTheme;
-
-  console.log("themeStyle", themeStyle);
+  const Component = props.component
+  const themeStyle = props.theme ? props.theme : myTheme
 
   return (
     <ThemeProvider theme={themeStyle}>
       <Component />
     </ThemeProvider>
-  );
-};
+  )
+}
 
-export default LeonTheme;
+export default LeonTheme
